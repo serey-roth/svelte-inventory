56480adc1ca87ec8e41fc5365f4a32cd966f511f
Serey Roth <sereyratanakroth@gmail.com>
2023-05-08 21:02:56 -0700

    refactor: use store to manage items

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
   import "./app.css";
+  import ItemCard from "./lib/ItemCard.svelte";
   import NewItemForm from "./lib/NewItemForm.svelte";
+  import { items } from "./lib/store";
   import type { Item } from "./types";
 
-  let items: Item[] = [
-    {
-      name: "Gloves",
-      quantity: 1,
-    },
-  ]
-
-  $: total = items.reduce((initalTotal, item) => initalTotal + item.quantity, 0);
-
-  const addNewItem = (newItem: Item) => {
-    const index = items.findIndex(item => item.name === newItem.name);
-    if (index === -1) {
-      items = [...items, newItem];
-    } else {
-      items[index].quantity += newItem.quantity;
-      items = items;
-    }
-  }
-
-  const handleAddNewItem = (event: CustomEvent<any>) => {
-    console.log(event)
-    addNewItem(event.detail);
-  }
+  $: total = $items.reduce(
+    (initalTotal, item) => initalTotal + item.quantity,
+    0
+  );
 </script>
 
 <main class="flex justify-center bg-yellow-500 w-screen min-h-screen pt-4">
@@ -34,24 +17,25 @@
     <p class="font-sm text-lg">
       We're using Svelte to create and manage an inventory.
     </p>
-    <NewItemForm on:add={handleAddNewItem}/>
+    <NewItemForm />
     <div class="mt-4 bg-white max-w-[500px] rounded-md p-2 drop-shadow-md">
-      <h3 class="font-semibold text-lg underline">Current Inventory</h3>
-      <ul class="list-disc">
-        {#each items as item (item.name)}
-          <li class="ml-4">
-            <span class="flex items-center">
-              <span class="truncate max-w-full">{item.name}</span>
-              <span class="ml-auto">X {item.quantity}</span>
-            </span>
-          </li>
-        {/each}
-      </ul>
-      <hr class="mt-2">
       <div class="flex items-center">
-        Total
-        <span class="ml-auto">{total}</span>
+        <h3 class="font-semibold text-lg">Current Inventory</h3>
+        <span class="ml-auto">Total: {total}</span>
       </div>
+      <hr class="my-2" />
+      {#if $items.length > 0}
+        <div class="flex flex-col w-full py-1 gap-2 max-h-[500px] overflow-y-auto scroll-smooth">
+          {#each $items as item, index (item)}
+            <ItemCard index={index}>
+              <span class="font-semibold truncate max-w-[50%]" slot="name">{item.name}</span>
+              <span slot="quantity" class="ml-1.5 rounded-lg px-2 bg-indigo-500/50 text-white">{item.quantity}</span>
+            </ItemCard>
+          {/each}
+        </div>
+      {:else}
+        <p class="text-red-500">Inventory is currently empty. Please add items!</p>
+      {/if}
     </div>
   </div>
 </main>
--- src/lib/NewItemForm.svelte
+++ src/lib/NewItemForm.svelte
@@ -1,31 +1,59 @@
 <script lang="ts">
   import App from "../App.svelte";
   import type { Item } from "../types";
-  import { createEventDispatcher } from "svelte";
+  import { items, editStatus } from "./store";
+  import { editableItem } from "./store";
 
   let name: string;
   let quantity: number = 1;
 
-  const dispatch = createEventDispatcher();
-  
+  $: {
+    if ($editableItem && $editStatus === 'preparing') {
+      name = $editableItem.name;
+      quantity = $editableItem.quantity;
+      editStatus.set('editing')
+    }
+  }
+
   const handleAddItem = () => {
-    dispatch('add', {
-      name,
-      quantity
+    items.update((items) => {
+      const index = items.findIndex(item => item.name === name);
+      if (index !== -1) {
+        items[index].quantity += quantity;
+        return items;
+      } else {
+        return [...items, { name, quantity }];
+      }
     });
-  };
+    name = "";
+    quantity = 1;
+  }
+
+  const handleEditItem = () => {
+    items.update((items) => {
+      if ($editableItem === undefined) return items;
+      items[$editableItem.index].name = name;
+      items[$editableItem.index].quantity = quantity;
+      return items;
+    });
+    editableItem.set(undefined);
+    editStatus.set('done');
+    name = "";
+    quantity = 1;
+  }
+
 </script>
 
-<form on:submit|preventDefault={handleAddItem}>
+<form on:submit|preventDefault={$editStatus === 'editing' && $editableItem ? handleEditItem : handleAddItem}>
   <div class="flex flex-col mb-2">
     <label for="name">Name</label>
-    <input bind:value={name} type="text" name="name" id="name"/>
+    <input bind:value={name} type="text" name="name" id="name" />
   </div>
   <div class="flex flex-col mb-2">
     <label for="quantity">Quantity</label>
     <input bind:value={quantity} type="number" name="quantity" id="quantity" min={1} max={50}/>
   </div>
-  <button class="mt-2 bg-gray-50 py-1 px-2 rounded-md" type="submit">Add Item</button>
+  <button class="mt-2 bg-gray-50 py-1 px-2 rounded-md" type="submit">{$editStatus === 'editing' ? 'Edit Item' : 'Add Item'}</button>
 </form>
 
 <style lang="postcss">
